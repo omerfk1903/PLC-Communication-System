0db2864e92736cd526686ab94c47a3ccea7cea26
Ömer Faruk Kılıç <39456101+omerfk1903@users.noreply.github.com>
2025-12-30 13:08:55 +0300

    Update

--- PLC_Python_Communication.py
+++ PLC_Python_Communication.py
@@ -3,63 +3,90 @@ from pyModbusTCP.client import ModbusClient
 from time import sleep 
 from random import randint
 from datetime import datetime as dt
+from logger import setup_logger
 
-CONTROL = 2
+# Tanımlar
+CONTROL = True
 register_address = 0
 last_rand = 0
+last_recv = 0
+step = 0
+Data_send_control = 0
+
+# Data is tn send 
+send = [None,None,None]# Value , rand, processing
+send[2] = 0
 
 # it is necssery value HOST and PORT for PLC connect 
 HOST = "192.168.0.1"
 PORT = 502
+
+# log is create
+logger = setup_logger(" PLC_Python_Com")
 
 # client is create
 client = ModbusClient(
     host=HOST,  
     port=PORT,
     auto_open=True,
-    auto_close=True
-)
+    auto_close=True)
 
-while True :
+while CONTROL :
 
-    if CONTROL == 1 : 
+    date = dt.now()# date
+
+    try : # recv
+
+        if step == 2 :
         
-        try : 
-        
-            recv = client.read_holding_registers(0, 10) # Data is read 
+            recv = client.read_holding_registers(0,3) # Data is read 
 
-            if recv : print("Client read:", recv)
+            Data_send_control = 0
+            Data_recv_control = 0
 
-        except : print("Read is error or data is not have")
+            if recv and recv[2] == 1  : 
+                
+                logger.info(f"✅ Data is recv : {str(recv)}")
+                
+                step = 0 # İŞLEM SIRASI
 
-    else : 
-        
-        register_address = 0 
+                send[2] = 0 # PLC İLETİŞİM SIRASI
 
-        value_to_send = int(input(" Data to be send : ")) # it will a take ınput
+    except : 
+         
+        logger.warning("Read is error or data is not have")
+
+        CONTROL = False
+
+    try : # send
 
         rand = randint(0,100) # it is a create number random
 
-        send = [value_to_send,rand] # it will a send the value
+        if last_rand != rand :
+        
+            if step == 0 and send[2] == 0 : 
 
-        if last_rand != rand : result = client.write_multiple_registers(register_address,send) # it is send data a lot
+                value_to_send = int(input(" Data to be send : ")) # it will a take ınput
 
-        else :
+                # it will a send the value
+                send[0] = value_to_send 
+                send[1] = rand
+            
+                result = client.write_multiple_registers(register_address,send) # it is send data a lot
 
-            for x in range(0,10) :
-
-                rand = randint(0,100)
-
-                if rand != last_rand :
-
-                    result = client.write_multiple_registers(register_address,[value_to_send,rand]) # it is send data a lot
-
-                    break
+                step = 2 # İŞLEM SIRASI 
+                Data_send_control = 1 # Print processing
                  
-        if result :  print(f"✅ {dt.now()}  Data is send : ", send)
-
-        else : print("❌ Data is not send")
+            if result and Data_send_control == 1 : logger.info(f"✅ Data is send : {str(send)}", )
 
         last_rand = rand 
 
+    except : 
+         
+        logger.warning(" Send is create error block ")
+
+        CONTROL = False
+
     sleep(0.5)
+
+if CONTROL == False : logger.warning(" Data is error send or recv ")
